ffd423b22cbe59c976ac18521bb66711a7b01aad
Sara Adam de Souza <saradesouza87@gmail.com>
2016-10-08 12:31:27 +0800

    Added php to footer

---
+++ footer.php
@@ -0,0 +1,1 @@
+<p>V3.0 - Designed and developed by Sara de Souza &copy; 2016</p>
---
+++ lib/footer.php
@@ -0,0 +1,18 @@
+<div id="footer">
+			<h2> About Me</h2>
+			<h6>Multimedia and Graphic designer<br>Selangor, Malaysia<br>Rabbit year, water goat month</h6>
+			<br>
+			<h4>Follow me!</h4>
+			<a href="https://www.behance.net/saradesouza" target="_blank"><img src="assets/socialmedia/behance.png"></a>
+			<a href="https://vimeo.com/saradesouza" target="_blank"><img src="assets/socialmedia/vimeo.png"></a>
+			<a href="https://my.linkedin.com/pub/sara-adam-de-souza/27/11a/56" target="_blank"><img src="assets/socialmedia/linkedin.png"></a>
+			<a href="mailto:saradesouza87@gmail.com"><img src="assets/socialmedia/email.png"></a>
+			<br><br>
+
+			<div id="footertxt">
+				<p>V3.0 - Designed and developed by Sara de Souza &copy; 2016</p>
+			</div>
+			<!-- <p>V3.0 - Designed and developed by Sara de Souza &copy; 2016</p> -->
+		</div>
+
+
